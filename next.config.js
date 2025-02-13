132f1874800d145c538edc724c7677c0aa89670c
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-13 20:29:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,46 +1,31 @@
 const webpack = require('webpack');
-// const path = require('path');
-const isProd = (process.env.NODE_ENV || 'production') === 'production';
-const assetPrefix = isProd ? '' : '';
+
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'spider_ospo'; 
+const assetPrefix = isProd ? `/${repoName}` : '';
+const basePath = isProd ? `/${repoName}` : '';
 
 module.exports = {
-  // future: {
-  //   webpack5: true,
-  // },
-  // node: {
-  //   __dirname: true,
-  //   __filename: true,
-  // },
-  // resolve: {
-  //   alias: {
-  //     src: path.resolve(__dirname, 'src'),
-  //     test: path.resolve(__dirname, 'test'),
-  //   },
-  // },
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true, // Fixes <Image /> component issues on GitHub Pages
+  },
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:5]',
     url: false,
   },
-  // exportPathMap() {
-  //   return {
-  //     '/src/pages/': { page: '/' },
-  //     '/src/pages/blog': { page: '/blog' },
-  //     '/src/pages/community': { page: '/community' },
-  //     '/src/pages/events': { page: '/events' },
-  //     '/src/pages/people': { page: '/people' },
-  //     '/src/pages/projects': { page: '/projects' },
-  //   };
-  // },
-  assetPrefix,
   webpack: (config) => {
     config.plugins.push(
       new webpack.DefinePlugin({
         'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
       }),
     );
+
     config.resolve.modules.push(__dirname);
+
     config.module.rules.push({
       test: /\.svg$/,
       use: [
