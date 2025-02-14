3667d11667ff2712544bc2f093086f0ff6bbed3f
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-14 12:16:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,21 @@
 const isProd = process.env.NODE_ENV === "production";
-const repoName = "spider_ospo"; // Make sure this matches your GitHub repo name!
+const repoName = "spider_ospo"; 
 
 module.exports = {
-  assetPrefix: isProd ? `/${repoName}` : "",
+  reactStrictMode: true,
+  assetPrefix: isProd ? `/${repoName}/` : "",
   basePath: isProd ? `/${repoName}` : "",
   trailingSlash: true,
   images: {
-    unoptimized: true, // Needed for GitHub Pages support
+    unoptimized: true, // This is needed for static exports
   },
-  webpack: (config) => {
-    config.resolve.modules.push(__dirname);
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, 
+        path: false,
+      };
+    }
     return config;
-  },
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/projects": { page: "/projects" },
-      "/community": { page: "/community" },
-      "/cybersecurity_research": { page: "/cybersecurity_research" },
-    };
   },
 };
