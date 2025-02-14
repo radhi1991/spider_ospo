3098a691f3fdc4e01992b478e87b5cee1cfe842c
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-14 12:10:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,34 +1,23 @@
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'spider_ospo';
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "spider_ospo"; // Make sure this matches your GitHub repo name!
 
 module.exports = {
-  assetPrefix: isProd ? `/${repoName}` : '',
-  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}` : "",
+  basePath: isProd ? `/${repoName}` : "",
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, // Needed for GitHub Pages support
   },
   webpack: (config) => {
     config.resolve.modules.push(__dirname);
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            icon: true,
-          },
-        },
-      ],
-    });
     return config;
   },
   exportPathMap: async function () {
     return {
-      '/': { page: '/' },
-      '/projects': { page: '/projects' },
-      '/community': { page: '/community' },
-      '/cybersecurity_research': { page: '/cybersecurity_research' },
+      "/": { page: "/" },
+      "/projects": { page: "/projects" },
+      "/community": { page: "/community" },
+      "/cybersecurity_research": { page: "/cybersecurity_research" },
     };
   },
 };
