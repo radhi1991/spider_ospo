0475436e221e4949764a917755bc10117dfc3cf9
R.Adhithya <33666474+radhi1991@users.noreply.github.com>
2025-02-25 12:25:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,14 @@
 const webpack = require('webpack');
-// const path = require('path');
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 const assetPrefix = isProd ? '' : '';
 
 module.exports = {
-  // future: {
-  //   webpack5: true,
-  // },
-  // node: {
-  //   __dirname: true,
-  //   __filename: true,
-  // },
-  // resolve: {
-  //   alias: {
-  //     src: path.resolve(__dirname, 'src'),
-  //     test: path.resolve(__dirname, 'test'),
-  //   },
-  // },
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:5]',
     url: false,
   },
-  // exportPathMap() {
-  //   return {
-  //     '/src/pages/': { page: '/' },
-  //     '/src/pages/blog': { page: '/blog' },
-  //     '/src/pages/community': { page: '/community' },
-  //     '/src/pages/events': { page: '/events' },
-  //     '/src/pages/people': { page: '/people' },
-  //     '/src/pages/projects': { page: '/projects' },
-  //   };
-  // },
   assetPrefix,
   webpack: (config) => {
     config.plugins.push(
@@ -41,22 +17,29 @@ module.exports = {
       }),
     );
     config.resolve.modules.push(__dirname);
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'babel-loader',
-        },
-        {
-          loader: 'react-svg-loader',
-          options: {
-            jsx: true, // true outputs JSX tags
+    config.module.rules.push(
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'babel-loader',
           },
-        },
-      ],
-    });
-
-    return config;
+          {
+            loader: 'react-svg-loader',
+            options: {
+              jsx: true, // true outputs JSX tags
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            return config;
   },
   devIndicators: {
     autoPrerender: false,
